192bf036e4f0a87b35e5ee4e76045865d0e43056
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-02-23 14:02:10 +0100

    improved partner font and algnment

--- src/components/partner.jsx
+++ src/components/partner.jsx
@@ -3,15 +3,15 @@ import { Icons } from "../assets";
 const Partner = () => {
   return (
     <div className="mt-[7rem]">
-      <h1 className="text-[white] text-center  text-[35px] lg:text-[50.46px] lg:leading-[50px] font-[500]  capitalize gradient mb-[5rem]">
+      <h1 className="text-[white] text-center  text-[25px] lg:text-[50.46px] lg:leading-[50px] font-[500]  capitalize gradient mb-[5rem]">
         we partner with
       </h1>
 
       <section className="flex  flex-row  gap-[2rem]  items-center justify-between">
         {/* ----partner 1---- */}
-        <div className="flex flex-col gap items-center mt-[30px] md:mt-[0]">
+        <div className="flex flex-col gap items-center  md:mt-[0]">
           <img src={Icons.coin} alt="rango" />
-          <p className="text-[white] leading-[35px] lg:leading-[40px] pt-[1rem] lg:text-[20.23px] font-[400] lg:w-[60%] pl-[20px] text-center">
+          <p className="text-[white] leading-[20px] lg:leading-[40px] pt-[1rem] lg:text-[20.23px] font-[400] lg:w-[60%] pl-[20px] text-center">
             coin official
           </p>
         </div>
