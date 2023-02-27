86bebb7c4b7773c346e977145577e93b0b830f63
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-02-27 02:24:07 +0100

    fixed menu

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,7 +1,7 @@
 import { Icons } from "../assets";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
+import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
