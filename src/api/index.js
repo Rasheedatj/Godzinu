59d14d1270e46998f4ce90cd1eaa36096db0e4fe
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-03-02 15:32:36 +0100

    team padding

--- src/api/index.js
+++
@@ -1,1 +0,0 @@
-// This is where I will keep all apis
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -6,16 +6,16 @@ import Earn from "../components/earn";
 import Hexagon from "../components/hexagon";
 
 const HomePage = () => {
-    const offset = 150
-    return (
-        <DefautLayout>
-            <Chain offset={offset} />
-            <About offset={offset} />
-            <Swap offset={offset} />
-            <Earn offset={offset} />
-            <Hexagon offset={offset} />
-        </DefautLayout>
-    )
-}
+  const offset = 150;
+  return (
+    <DefautLayout>
+      <Chain offset={offset} />
+      <About offset={offset} />
+      <Swap offset={offset} />
+      <Earn offset={offset} />
+      <Hexagon offset={offset} />
+    </DefautLayout>
+  );
+};
 
-export default HomePage
+export default HomePage;
