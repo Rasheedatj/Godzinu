f7a886e3217e7bf7b86dc978b70a6857e1ec076a
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-03-04 09:07:54 +0100

    final commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,9 @@
 import anchor from "./anchor.svg";
-
 import bolt from "./bolt.svg";
-
 import close from "./close.svg";
 import cost from "./Costdollar.svg";
-
 import cube from "./cube.svg";
 import discord from "./discord.svg";
-
 import dragon3 from "./dragon3.svg";
 import easy from "./easy.svg";
 import facebook from "./facebook.svg";
@@ -16,34 +12,24 @@ import people from "./people.png";
 import footerimg from "./footerimg.svg";
 import github from "./github.svg";
 import godzilla from "./godzilla.svg";
-
 import icons from "./icons.svg";
-
 import minus from "./minus.svg";
 import instagram from "./instagram.svg";
-
 import rate from "./rate.svg";
 import reddit from "./reddit.png";
-
 import telegram from "./Telegram.svg";
 import twitter from "./twitter.svg";
 import twittericon from "./twittericon.svg";
 import vector from "./vector.svg";
 import vectoricon from "./Vectoricon.svg";
-
 export const Icons = {
   people,
-
   anchor,
-
   bolt,
-
   close,
   cost,
-
   cube,
   discord,
-
   dragon3,
   easy,
   flash,
@@ -51,15 +37,11 @@ export const Icons = {
   footerimg,
   github,
   godzilla,
-
   icons,
-
   minus,
   instagram,
-
   rate,
   reddit,
-
   telegram,
   twitter,
   twittericon,
